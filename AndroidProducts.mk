eb0d81bca129a690ea01c3195a0d93d64a0ef69f
wayneeg <waynee@outlook.com.br>
2021-08-31 13:24:42 +0000

    wavefy
    Signed-off-by: wayneeg <waynee@outlook.com.br>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2021 The waveOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_alioth.mk
+    $(LOCAL_DIR)/wave_alioth.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_alioth-user \
-    lineage_alioth-userdebug \
-    lineage_alioth-eng
+    wave_alioth-user \
+    wave_alioth-userdebug \
+    wave_alioth-eng
